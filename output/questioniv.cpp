86eb52d09231a37c7946bad905e2874d0e88b0ee
Dishita Singh <dishita160306@gmail.com>
2026-09-16 10:32:20 +0530

    Question 4

---
+++ output/questioniv.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class University
+{
+private:
+    string universityName;
+
+public:
+    University(string name)
+    {
+        universityName = name;
+    }
+
+    class Department
+    {
+    private:
+        string departmentName;
+        int studentCount;
+
+    public:
+        Department(string dept, int count)
+        {
+            departmentName = dept;
+            studentCount = count;
+        }
+
+        void display(University &u)
+        {
+            cout << "University Name: " << u.universityName << endl;
+            cout << "Department Name: " << departmentName << endl;
+            cout << "Student Count: " << studentCount << endl;
+        }
+    };
+};
+
+int main()
+{
+    University u("ABES Engineering College");
+
+    University::Department d("CSE-AIML", 60);
+
+    d.display(u);
+
+    return 0;
+}
